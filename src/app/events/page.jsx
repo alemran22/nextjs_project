e70c30e9df9d262c069a130f32941de3cb3bba66
Al Emran <alemrangazi@gmail.com>
2024-10-08 23:52:18 +0600

    event page mobile responsive design and routing path created

---
+++ src/app/events/page.jsx
@@ -0,0 +1,12 @@
+import EventPage from "@/components/EventPage.jsx";
+import React from "react";
+
+const Events = () => {
+  return (
+    <div>
+      <EventPage />
+    </div>
+  );
+};
+
+export default Events;
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,8 @@ module.exports = {
         "banner-img": "url('/banner.png')",
         "background-img": "url('/background.png')",
         "event-banner-bg": "url('/event-img/event-banner.png')",
+        "event-footer-bg": "url(/event-img/Footer.png)",
+        "landing-footer-bg": "url(/Footer.png)",
       },
       fontFamily: {
         phudu: ["Phudu", "sans-serif"],
